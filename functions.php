f9e19ee5c89525b21696b882800184038b395831
Habib Rahaman <habibjh88@gmail.com>
2025-11-11 00:06:58 +0600

    blor archive

--- functions.php
+++ functions.php
@@ -1,4 +1,8 @@
 <?php
+// Include WPBakery custom elements
+require_once get_stylesheet_directory() . '/wpbakery/init.php';
+require_once get_stylesheet_directory() . '/inc/meta.php';
+require_once get_stylesheet_directory() . '/inc/customizer.php';
 
 add_action( 'wp_enqueue_scripts', 'musea_child_styles' );
 function musea_child_styles() {
@@ -32,10 +36,7 @@ function musea_child_styles() {
 
 	wp_enqueue_style( 'swiper-css', get_stylesheet_directory_uri() . '/assets/lib/swiper-bundle.min.css', [], '11.0.0' );
 	wp_enqueue_script( 'swiper-js', get_stylesheet_directory_uri() . '/assets/lib/swiper-bundle.min.js', [], '11.0.0', true );
-
-
 	wp_enqueue_script( 'neuzin-appear', get_stylesheet_directory_uri() . '/assets/js/scripts.js', [ 'jquery' ], 1.4, true );
-
 }
 
 
@@ -51,7 +52,90 @@ add_filter( 'body_class', function ( $classes ) {
 	return $classes;
 } );
 
-// Include WPBakery custom elements
-require_once get_stylesheet_directory() . '/wpbakery/init.php';
-require_once get_stylesheet_directory() . '/inc/meta.php';
-require_once get_stylesheet_directory() . '/inc/customizer.php';
+
+if ( ! function_exists( 'musea_elated_get_sticky_post' ) ) {
+	/**
+	 * Function which create query for blog lists
+	 *
+	 * @param $type string with name of list that is loaded
+	 */
+	function musea_elated_get_sticky_post( $type ) {
+		$sticky = get_option( 'sticky_posts' );
+
+		$sticky_post_id = ! empty( $sticky[0] ) ? $sticky[0] : '';
+
+		if ( ! $sticky_post_id ) {
+			return;
+		}
+		global $musea_exclude_post;
+		$musea_exclude_post = $sticky_post_id;
+
+		$args = array(
+			'posts_per_page'      => 1,
+			'post__in'            => [$sticky_post_id],
+			'ignore_sticky_posts' => 1
+		);
+
+		$sticky_post = new WP_Query( $args );
+
+		$max_num_pages = $sticky_post->max_num_pages;
+
+		$blog_classes     = musea_elated_get_blog_list_holder_classes( $type );
+		$blog_data_params = musea_elated_get_blog_holder_data_params( $type );
+
+		$params = array(
+			'blog_query'       => $sticky_post,
+			'max_num_pages'    => $max_num_pages,
+			'blog_type'        => $type,
+			'blog_classes'     => $blog_classes,
+			'blog_data_params' => $blog_data_params
+		);
+
+		musea_elated_get_module_template_part( 'templates/lists/' . $type . '/list', 'blog', '', $params );
+	}
+}
+
+if ( ! function_exists( 'musea_elated_get_blog_query_child' ) ) {
+	/**
+	 * Function which create query for blog lists
+	 *
+	 * @return wp query object
+	 */
+	function musea_elated_get_blog_query_child() {
+		$id                       = musea_elated_get_page_id();
+		$category                 = esc_attr( get_post_meta( $id, 'eltdf_blog_category_meta', true ) );
+		$number_of_posts_per_page = get_post_meta( $id, 'eltdf_show_posts_per_page_meta', true );
+		$post_number              = ! empty( $number_of_posts_per_page ) ? esc_attr( $number_of_posts_per_page ) : esc_attr( get_option( 'posts_per_page' ) );
+
+		if ( get_query_var( 'paged' ) ) {
+			$paged = get_query_var( 'paged' );
+		} elseif ( get_query_var( 'page' ) ) {
+			$paged = get_query_var( 'page' );
+		} else {
+			$paged = 1;
+		}
+
+		global $musea_exclude_post;
+
+		$query_array = array(
+			'post_status'    => 'publish',
+			'post_type'      => 'post',
+			'paged'          => $paged,
+			'category_name'  => $category,
+			'posts_per_page' => $post_number,
+		);
+
+		if($musea_exclude_post) {
+			$query_array['exclude'] = [$musea_exclude_post];
+			$query_array['ignore_sticky_post'] = 1;
+		}
+
+		$blog_query = new WP_Query( $query_array );
+		if ( is_archive() ) {
+			global $wp_query;
+			$blog_query = $wp_query;
+		}
+
+		return $blog_query;
+	}
+}
---
+++ home.php
@@ -0,0 +1,58 @@
+<?php
+$eltdf_blog_type = musea_elated_get_archive_blog_list_layout();
+musea_elated_include_blog_helper_functions( 'lists', $eltdf_blog_type );
+$eltdf_holder_params = musea_elated_get_holder_params_blog();
+global $musea_exclude_post;
+get_header();
+musea_elated_get_title();
+//musea_elated_get_blog( $eltdf_blog_type );
+$sidebar_layout   = musea_elated_sidebar_layout();
+$grid_space_meta  = musea_elated_get_meta_field_intersect( 'blog_list_grid_space', musea_elated_get_page_id() );
+$holder_classes   = array();
+$holder_classes[] = ! empty( $grid_space_meta ) ? 'eltdf-grid-' . $grid_space_meta . '-gutter' : 'eltdf-grid-large-gutter';
+$holder_classes   = apply_filters( 'musea_elated_filter_blog_holder_classes', $holder_classes );
+$holder_classes   = implode( ' ', $holder_classes );
+$blog_type        = $eltdf_blog_type;
+do_action( 'musea_elated_action_before_main_content' );
+?>
+
+    <div class="<?php echo esc_attr( $eltdf_holder_params['holder'] ); ?>">
+		<?php do_action( 'musea_elated_action_after_container_open' ); ?>
+
+        <div class="<?php echo esc_attr( $eltdf_holder_params['inner'] ); ?>">
+            <div class="eltdf-grid-row <?php echo esc_attr( $holder_classes ); ?>">
+				<?php musea_elated_get_sticky_post( $blog_type ) ?>
+            </div>
+            <div class="eltdf-grid-row <?php echo esc_attr( $holder_classes ); ?>">
+					<?php
+					$blog_query    = musea_elated_get_blog_query_child();
+					$paged         = isset( $blog_query->query['paged'] ) ? $blog_query->query['paged'] : 1;
+					$max_num_pages = $blog_query->max_num_pages;
+
+					$blog_classes     = musea_elated_get_blog_list_holder_classes( $blog_type );
+					$blog_data_params = musea_elated_get_blog_holder_data_params( $blog_type );
+
+					$params = array(
+						'blog_query'       => $blog_query,
+						'paged'            => $paged,
+						'max_num_pages'    => $max_num_pages,
+						'blog_type'        => $blog_type,
+						'blog_classes'     => $blog_classes,
+						'blog_data_params' => $blog_data_params,
+					);
+
+					musea_elated_get_module_template_part( 'templates/lists/' . $blog_type . '/list', 'blog', '', $params );6?>
+                </div>
+				<?php if ( $sidebar_layout == 'no-sidebar' ) { ?>
+                    <div <?php echo musea_elated_get_sidebar_holder_class(); ?>>
+						<?php get_sidebar(); ?>
+                    </div>
+				<?php } ?>
+            </div>
+        </div>
+
+		<?php do_action( 'musea_elated_action_before_container_close' ); ?>
+    </div>
+
+<?php do_action( 'musea_elated_action_blog_list_additional_tags' ); ?>
+<?php get_footer(); ?>
